08aefc194736491200e3984c33576bd16eaffac0
Seika <artistnoob6@gmail.com>
2026-01-20 01:23:32 +0530

    v0.1 : Adding the boiler plate

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Complete end to end MLOPS Project Helping in Screening Resume according to JD .',
+    author='sei',
+    license='',
+)
